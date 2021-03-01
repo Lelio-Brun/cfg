6b7f207869be42008a24db09afb9968c947854cf
Lélio Brun <Lelio-Brun@users.noreply.github.com>
2021-03-01 10:55:07 +0100

    Add files via upload

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ set fish_color_comment 888888
 set fish_color_autosuggestion $fish_color_comment
 
 abbr -a -g gco git checkout
-abbr -a -g gc  git clone
+abbr -a -g gcl git clone
 abbr -a -g gf  git fetch
 abbr -a -g gfm git pull
 abbr -a -g gp  git push
@@ -17,6 +17,7 @@ abbr -a -g gSt git status
 abbr -a -g gc  git commit --verbose
 abbr -a -g gcm git commit --verbose --message
 abbr -a -g gca git commit --verbose --all
+abbr -a -g grm git rm
 
 abbr -a -g oce 'eval (opam env)'
 abbr -a -g osw opam switch
@@ -24,6 +25,16 @@ abbr -a -g oup opam update
 abbr -a -g oug opam upgrade
 abbr -a -g oin opam install
 abbr -a -g opa opam pin add
+
+abbr -a -g dub dune build
+abbr -a -g dux dune exec
+abbr -a -g dui dune install
+abbr -a -g duc dune clean
+abbr -a -g dut dune runtest
+
+abbr -a -g o open
+
+source ~/.opam/opam-init/init.fish
 
 starship init fish | source
 kitty + complete setup fish | source
