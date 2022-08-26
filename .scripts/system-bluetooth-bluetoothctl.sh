556c8e51dabf412fb38f4ffb39b559dee21d4653
Lélio Brun <Lelio-Brun@users.noreply.github.com>
2022-08-26 17:27:17 +0200

    polybar scripts

--- .scripts/system-bluetooth-bluetoothctl.sh
+++ .scripts/system-bluetooth-bluetoothctl.sh
@@ -1,64 +1,25 @@
 #!/bin/sh
 
-bluetooth_print() {
-    bluetoothctl | while read -r REPLY; do
-        if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
-            devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
-            counter=0
+if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
+    devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
+    counter=0
 
-            for device in $devices_paired; do
-                device_info=$(bluetoothctl info "$device")
+    for device in $devices_paired; do
+        device_info=$(bluetoothctl info "$device")
 
-                if echo "$device_info" | grep -q "Connected: yes"; then
-                    device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
+        if echo "$device_info" | grep -q "Connected: yes"; then
+            device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
 
-                    if [ $counter -eq 0 ]; then
-                        printf '%%{F#F0C674}%%{F-}'
-                    fi
-                    if [ $counter -gt 0 ]; then
-                        printf ", %s" "$device_alias"
-                    else
-                        printf " %s" "$device_alias"
-                    fi
-
-                    counter=$((counter + 1))
-                fi
-            done
-
-            if [ $counter -eq 0 ]; then
-                printf '%%{F#F0C674}%%{F-}'
+            if [ $counter -gt 0 ]; then
+                printf ", %s" "$device_alias"
+            else
+                printf " %s" "$device_alias"
             fi
-            printf '\n'
-        else
-            printf '%%{F#707880}%%{F-}\n'
+
+            counter=$((counter + 1))
         fi
     done
-}
-
-bluetooth_toggle() {
-    if bluetoothctl show | grep -q "Powered: no"; then
-        bluetoothctl power on >> /dev/null
-        sleep 1
-
-        devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
-        echo "$devices_paired" | while read -r line; do
-            bluetoothctl connect "$line" >> /dev/null
-        done
-    else
-        devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
-        echo "$devices_paired" | while read -r line; do
-            bluetoothctl disconnect "$line" >> /dev/null
-        done
-
-        bluetoothctl power off >> /dev/null
-    fi
-}
-
-case "$1" in
-    --toggle)
-        bluetooth_toggle
-        ;;
-    *)
-        bluetooth_print
-        ;;
-esac
+    printf '‍\n' # use [U+200C Zero Width Non-Joiner] to trick polybar
+    exit
+fi
+exit 1
